bb6b522417cdfda52b737524d7b76dcf5a6a31e9
ma2arten <marten.ester@gmail.com>
2020-01-04 11:12:38 +0200

    prepending 0 in case month num is one digit

--- models/transactions.js
+++ models/transactions.js
@@ -4,6 +4,9 @@ var db  = require ("./database/db")
 function transactions(month, year){
 this.month = month;
 this.year = year;
+if (this.month.length === 1){
+  this.month = "0"+this.month;
+}
 
 this.getTransactions = function(){
   let monthStart = `${this.year}-${this.month}-01`
@@ -29,15 +32,15 @@ return `${this.year}-${this.month}-${this.daysInMonth()}`
 this.daysInMonth = function(){
  var feb = this.year % 4 == 0 ? 29 : 28
  let daysInMonthMap ={
-   "1":31,
-   "2":feb,
-   "3":31,
-   "4":30,
-   "5":31,
-   "6":30,
-   "7":31,
-   "8":31,
-   "9":30,
+   "01":31,
+   "02":feb,
+   "03":31,
+   "04":30,
+   "05":31,
+   "06":30,
+   "07":31,
+   "08":31,
+   "09":30,
    "10":31,
    "11":30,
    "12":31
