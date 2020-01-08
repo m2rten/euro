68dd805c74f0d6dee20f492448669eef296f3c00
ma2arten <marten.ester@gmail.com>
2020-01-08 12:55:04 +0200

    added totals

--- controllers/report/monthlyReportController.js
+++ controllers/report/monthlyReportController.js
@@ -25,9 +25,9 @@ this.getMonthlyReport = function (){
   promises.push(this.mrp.getBankPeriodSum("everyday","bank"));
   return Promise.all(promises)
   .then(function(values){
-    let plannedTotal = values[0].reduce((acc,b)=>acc + b.expense_daily_sum*daysInMonth(),0)
+    let plannedTotal = values[0].reduce((acc,b)=>acc + Math.round(b.expense_daily_sum*daysInMonth()),0)
     let planned = values[0].map(item=>{
-      return ({[item.expense_type]:item.expense_daily_sum*daysInMonth()})
+      return ({[item.expense_type]:Math.round(item.expense_daily_sum*daysInMonth())})
     });
     planned = planned.reduce((acc,b)=> Object.assign(acc,b),{})
     let cashStart = values[1];
@@ -36,24 +36,26 @@ this.getMonthlyReport = function (){
     let incomingCash = values[3];
     let refund = values[4];
     let everyday = Math.round((-1)*values [5]);
-    let totalSpent = cashDiff + everyday + incomingCash - refund
+    let everydaySpent = cashDiff + everyday + incomingCash - refund
     let daily = {};
-    daily.spent = Math.round(totalSpent/getToday())
+    daily.spent = Math.round(everydaySpent/getToday())
     daily.planned = Math.round(planned.everyday/daysInMonth());
-    daily.left = Math.round((planned.everyday - totalSpent)/(daysInMonth()-getToday()+1))
-    let monthlyTotalSpent  = plannedTotal + totalSpent - planned.everyday;
+    daily.left = Math.round((planned.everyday - everydaySpent)/(daysInMonth()-getToday()+1))
+    let totalSpent = everydaySpent + plannedTotal - planned.everyday;
+    let outcome = plannedTotal - totalSpent;
     let response ={
       "planned":planned,
       "plannedTotal":plannedTotal,
       "Spent Cash":cashDiff + incomingCash ,
       "refund":refund,
       "everyday":everyday,
-      "total Spent": everyday + plannedTotal - planned.everyday + cashDiff + incomingCash -refund,
+      "total Spent": totalSpent,
       "daily":{
         "spent":daily.spent,
         "planned":daily.planned,
         "left":daily.left
-      }
+      },
+      "outcome": outcome
     }
     return response
   })
